f77f2f4179e5890432821fa0430ba80cee153f0b
Mauricio <mauriciohillesheim15@gmail.com>
2024-11-29 18:21:14 -0300

    up

--- index.js
+++ index.js
@@ -3,27 +3,22 @@ const routesCliente = require("./src/routes/cliente")
 const routesFilme  = require("./src/routes/filme")
 const routesFilmeslocados = require ("./src/routes/filmeslocados")
 const database = require('./src/config/database')
-// instaciar um express
 
 const app= express()
-
-//middleware json - aceita json no body
 app.use(express.json())
 
-// Adicionar as rotas ao express
 app.use('/clientes',routesCliente)
 app.use('/filme',routesFilme)
 app.use('/filmeslocados',routesFilmeslocados)
 
 const PORT=3000
 
-//conexão com banco de dados;
-database.db 
+database.db
   .sync({ force: false })
   .then((_)  => {
     console.info("Banco conectado com sucesso")
 
-    app.listen(PORT, ( )=> { //usando o template screnn
+    app.listen(PORT, ( )=> { 
       console.info(`Servidor rodando a porta ${PORT}`)
     })
   })
